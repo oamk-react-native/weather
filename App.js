7193cf94881d631e5229780ba9c15d9a718242ef
Jouni Juntunen <jouni.juntunen@oamk.fi>
2020-12-15 16:21:53 +0200

    Geolocation working.

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [latitude,setLatitude] = useState(0);  
+  const [longitude,setLongitude] = useState(0); 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getLocation().then(() => {
+      setLoading(false);
+    })
+  }, [])
+
+  const getLocation = async () => {
+    const reply = await Permissions.askAsync(Permissions.LOCATION);
+    if (reply.granted) {
+      const location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.Highest});
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);  
+    }
+  };
+
+  if (loading) { 
+    return <View style={styles.container}><Text>Loading...</Text></View>
+  }
+  else {
+    return (
+      <View style={styles.container}>
+        <Text>Location:</Text>
+        <Text>{latitude}</Text>
+        <Text>{longitude}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
